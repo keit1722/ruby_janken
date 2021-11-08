5c3856238f5b8fe4d3bbe54499ef5978505cc126
keit1722 <keit1722@gmail.com>
2021-11-09 00:58:51 +0900

    じゃんけんプログラムを実装

--- janken.rb
+++ janken.rb
@@ -1,30 +1,29 @@
+# frozen_string_literal: true
+
 class Janken
   HAND = {
     g: {
       call: 'グー',
       against: {
-        g: 0,
         c: 1,
-        p: -1,
-      },
+        p: -1
+      }
     },
     c: {
       call: 'チョキ',
       against: {
         g: -1,
-        c: 0,
-        p: 1,
-      },
+        p: 1
+      }
     },
     p: {
       call: 'パー',
       against: {
         g: 1,
-        c: -1,
-        p: 0,
-      },
-    },
-  }
+        c: -1
+      }
+    }
+  }.freeze
 
   def initialize
     @result = { you: [], opponent: [] }
@@ -39,9 +38,9 @@ class Janken
   def select_times_for_game
     puts '何本勝負？(press 1 or 3 or 5)'
     selected_number = gets.to_i
-    if selected_number == 1 || selected_number == 3 || selected_number == 5
+    if [1, 3, 5].include?(selected_number)
       puts "#{selected_number}本勝負を選びました。"
-      times_for_game = selected_number
+      selected_number
     else
       puts '入力が間違っています。入力し直してください。'
       select_times_for_game
@@ -51,7 +50,7 @@ class Janken
   def play_game(start_with)
     puts "#{@result[:you].size + 1}本目"
     your_hand = select_your_hand(start_with)
-    opponents_hand = get_opponents_hand
+    opponents_hand = set_opponents_hand
     show_temporary_result(your_hand, opponents_hand)
     if hands_draw?(your_hand, opponents_hand)
       play_game('あいこで')
@@ -64,7 +63,7 @@ class Janken
   def select_your_hand(start_with)
     puts "#{start_with}…(press g or c or p)"
     your_hand = gets.chomp.to_sym
-    if your_hand == :g || your_hand == :c || your_hand == :p
+    if %i[g c p].include?(your_hand)
       your_hand
     else
       puts '入力が間違っています。入力し直してください。'
@@ -72,8 +71,8 @@ class Janken
     end
   end
 
-  def get_opponents_hand
-    opponents_hand = %i[g c p].sample
+  def set_opponents_hand
+    %i[g c p].sample
   end
 
   def hands_draw?(your_hand, opponents_hand)
@@ -91,14 +90,14 @@ class Janken
     else
       puts '負け！'
     end
-    puts get_score
+    puts show_score
   end
 
   def you_win?
-    @result[:you].sum > @result[:opponent].sum ? true : false
+    @result[:you].sum > @result[:opponent].sum
   end
 
-  def get_score
+  def show_score
     your_score = @result[:you]
     win_scores = your_score.select { |score| score == 1 }
     lose_scores = your_score.select { |score| score == -1 }
